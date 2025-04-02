b4785d40c49bb9511f1fa3e7dfbf80181ac8e754
simonpartridge86 <simonpartridge86@gmail.com>
2025-04-02 19:49:52 +0100

    deploy app to S3 bucket

--- lib/notes-app-cdk-stack.ts
+++ lib/notes-app-cdk-stack.ts
@@ -1,16 +1,30 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { RemovalPolicy } from "aws-cdk-lib";
+import { BlockPublicAccess, Bucket } from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { Construct } from "constructs";
 
 export class NotesAppCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'NotesAppCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Creates an S3 bucket to host the app
+    const bucket = new Bucket(this, "NotesAppBucket", {
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true, // Allows public access for static website
+      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS, // Allow public access through bucket policies
+      removalPolicy: RemovalPolicy.DESTROY, // Use RemovalPolicy.RETAIN in production
+      autoDeleteObjects: true, // Remove this in production
+    });
+    // Deploys the app to the S3 bucket
+    new BucketDeployment(this, "DeployReactApp", {
+      sources: [Source.asset("../notes-app/dist")],
+      destinationBucket: bucket,
+      retainOnDelete: false, // Ensure the files are deleted with the bucket
+    });
+    // Outputs the URL of the website
+    new cdk.CfnOutput(this, "WebsiteURL", {
+      value: bucket.bucketWebsiteUrl,
+    });
   }
 }
